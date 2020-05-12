925d8d40427b3631203e487343c65f947c54b42d
Adam Botma <adam.botma@me.com>
2020-05-11 17:40:52 -0700

    started with api call to get card info, created a class for cards, fill cards with data, fill deck with cards

---
+++ deckbuild.js
@@ -0,0 +1,13 @@
+class Card {
+  constructor (name, species, image, strength){
+    this.name = name
+    this.species = species
+    this.image = image
+    this.strength = strength
+  }
+
+
+
+
+
+}
